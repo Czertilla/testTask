c53533d640726cef16a191416064310d44bc0b60
Czertilla <blaztermapskorsak@gmail.com>
2024-09-17 17:14:14 +0300

    feature(urls) add router edit module libabook.store.urls:     add var router

--- libabook/store/urls.py
+++ libabook/store/urls.py
@@ -2,7 +2,10 @@ from .views import index
 from django.urls import path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
+from rest_framework.routers import DefaultRouter
 from drf_yasg import openapi
+
+router = DefaultRouter()
 
 schema_view = get_schema_view(
    openapi.Info(
