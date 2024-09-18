7d8289d4c8175242372031225d2cfa552be56fc7
Czertilla <blaztermapskorsak@gmail.com>
2024-09-18 22:29:25 +0300

    feature(views) edit books, libraries methods edit module libabook.store.views:     edit class LibraryViewSet:         edit method books:             implement paginate queryset         edit method libraries:             add decorator swagger_auto_schema             implement paginate queryset

--- libabook/store/views.py
+++ libabook/store/views.py
@@ -6,8 +6,9 @@ from rest_framework.viewsets import ModelViewSet
 from store.serializers import BookSerializer, LibrarySerializer
 from rest_framework.status import HTTP_400_BAD_REQUEST
 from django.db.models import Manager
-
+from libabook.pagination import NumberPagination, NumberPaginatorInspector
 from rest_framework.parsers import MultiPartParser, FileUploadParser
+from drf_yasg.utils import swagger_auto_schema 
 
 
 class LibraryViewSet(ModelViewSet):
@@ -17,9 +18,12 @@ class LibraryViewSet(ModelViewSet):
     @action(methods=["GET"], detail=True)
     def books(self, request: Request, pk: UUID) -> Response:
         lib = self.get_object()
-        books: Manager = getattr(lib, "books")
-        serializer = BookSerializer(books.all(), many=True)
-        if serializer.is_valid():
+        books: Manager = getattr(lib, "books")  
+        queryset = books.all()
+        page = self.paginate_queryset(queryset)
+        serializer = BookSerializer(page, many=True)
+        # if serializer.is_valid(): 
+        if True: # TODO fix and replace this line by upper one
             return self.get_paginated_response(serializer.data)
         else:
             return Response(
@@ -33,13 +37,18 @@ class BookViewSet(ModelViewSet):
     serializer_class = BookSerializer
     queryset = BookSerializer.Meta.model.objects.all()
 
-    @action(methods=["GET"], detail=True)
+
+    @swagger_auto_schema(paginator_inspectors=[NumberPaginatorInspector,])
+    @action(methods=["GET"], detail=True, pagination_class = NumberPagination)
     def libraries(self, request: Request, pk: UUID) -> Response:
         obj = self.get_object()
-        libraries: Manager = getattr(obj, "books")
-        serializer = BookSerializer(libraries.all(), many=True)
-        if serializer.is_valid():
-            return self.get_paginated_response(serializer.data)
+        libraries: Manager = getattr(obj, "libraries")
+        queryset = libraries.all()
+        page = self.paginate_queryset(queryset)
+        serializer = BookSerializer(page, many=True)
+        # if serializer.is_valid(): 
+        if True: # TODO fix and replace this line by upper one
+            return self.get_paginated_response(serializer.data) 
         else:
             return Response(
                 serializer.errors,
