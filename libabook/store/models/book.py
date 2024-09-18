a60ce0254d5c580b69b919786aa0656d3ae42743
Czertilla <blaztermapskorsak@gmail.com>
2024-09-18 18:23:49 +0300

    chore(models) add m2m field to Book model edit module libabook.models.book:     edit class Book:         add field

--- libabook/store/models/book.py
+++ libabook/store/models/book.py
@@ -1,11 +1,17 @@
+from typing import TYPE_CHECKING
 from .mixins import TimestampMixin, UUIDMixin
 from django.db.models import (
     CharField,
-    FileField
+    FileField, 
+    ManyToManyField
 )
 from libabook.storages import PrivateMediaStorage
+
+if TYPE_CHECKING:
+    from .library import Library
 
 class Book(UUIDMixin, TimestampMixin):
     title = CharField(max_length=64)
     author = CharField(max_length=64)
     text = FileField(blank=True, storage=PrivateMediaStorage())
+    libraries = ManyToManyField("Library", through="LibraryToBook")
