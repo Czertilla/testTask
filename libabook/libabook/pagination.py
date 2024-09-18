3810ec04c3065a1110d5f8c7a3074a08affe661e
Czertilla <blaztermapskorsak@gmail.com>
2024-09-18 22:28:37 +0300

    feature(pagination) add NumberPaginatorInspector edit module libabook.pagination:     add class NumberPaginatorInspector

--- libabook/libabook/pagination.py
+++ libabook/libabook/pagination.py
@@ -5,6 +5,8 @@ from dataclasses_json import dataclass_json
 from typing import Any
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
+from drf_yasg import openapi
+from drf_yasg.inspectors import PaginatorInspector
 
 
 @dataclass_json
@@ -34,3 +36,26 @@ class NumberPagination(PageNumberPagination):
         ).to_dict()
 
         return Response(pagination_response)
+    
+class NumberPaginatorInspector(PaginatorInspector):
+
+    def get_paginated_response(self, paginator, response_schema):
+        """
+        :param BasePagination paginator: the paginator
+        :param openapi.Schema response_schema: the response schema that must be paged.
+        :rtype: openapi.Schema
+        """
+
+        return openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties=openapi.OrderedDict((
+                ('count', openapi.Schema(type=openapi.TYPE_INTEGER)),
+                ('next', openapi.Schema(type=openapi.TYPE_INTEGER)),
+                ('previous', openapi.Schema(type=openapi.TYPE_INTEGER)),
+                ('page_number', openapi.Schema(type=openapi.TYPE_INTEGER)),
+                ('num_of_pages', openapi.Schema(type=openapi.TYPE_INTEGER)),
+                ('page_size', openapi.Schema(type=openapi.TYPE_INTEGER)),
+                ('results', response_schema)
+            )),
+            required=['results']
+        )
