4b70e336009e588bd8d24acec67e6fcdbcdee1c5
Czertilla <blaztermapskorsak@gmail.com>
2024-09-18 18:25:41 +0300

    feature(views) add libraries and books methods edit module libabook.store.views:     edit class LibraryViewSet:         add method libraries:             implement getting list of objs from m2m field             implement serializer validation on gen response     edit class BookViewSet:         add method libraries:             implement getting list of objs from m2m field             implement serializer validation on gen response

--- libabook/store/views.py
+++ libabook/store/views.py
@@ -1,19 +1,47 @@
-from os import getenv
-from rest_framework.decorators import api_view
+from uuid import UUID
+from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.request import Request
 from rest_framework.viewsets import ModelViewSet
 from store.serializers import BookSerializer, LibrarySerializer
+from rest_framework.status import HTTP_400_BAD_REQUEST
+from django.db.models import Manager
 
 from rest_framework.parsers import MultiPartParser, FileUploadParser
-from django.shortcuts import get_object_or_404
+
 
 class LibraryViewSet(ModelViewSet):
     serializer_class = LibrarySerializer
-    queryset = LibrarySerializer.Meta.model.objects.all()
+    queryset = serializer_class.Meta.model.objects.all()
+
+    @action(methods=["GET"], detail=True)
+    def books(self, request: Request, pk: UUID) -> Response:
+        lib = self.get_object()
+        books: Manager = getattr(lib, "books")
+        serializer = BookSerializer(books.all(), many=True)
+        if serializer.is_valid():
+            return self.get_paginated_response(serializer.data)
+        else:
+            return Response(
+                serializer.errors,
+                status=HTTP_400_BAD_REQUEST
+            )
 
 
 class BookViewSet(ModelViewSet):
     parser_classes = [MultiPartParser, FileUploadParser]
     serializer_class = BookSerializer
     queryset = BookSerializer.Meta.model.objects.all()
+
+    @action(methods=["GET"], detail=True)
+    def libraries(self, request: Request, pk: UUID) -> Response:
+        obj = self.get_object()
+        libraries: Manager = getattr(obj, "books")
+        serializer = BookSerializer(libraries.all(), many=True)
+        if serializer.is_valid():
+            return self.get_paginated_response(serializer.data)
+        else:
+            return Response(
+                serializer.errors,
+                status=HTTP_400_BAD_REQUEST
+            )
