db2d50d1af0993bc0c85b8f0ae37f3ee70e91026
Czertilla <blaztermapskorsak@gmail.com>
2024-09-19 01:00:29 +0300

    feature(libabook.urls) intergrate drf-spectacular edit module libabook.urls:     edit list urlpatterns:         add paths:             SpectacularAPIView             SpectacularSwaggerView

--- libabook/libabook/urls.py
+++ libabook/libabook/urls.py
@@ -21,6 +21,8 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 urlpatterns = [
     path('store/', include("store.urls")),
     path('admin/', admin.site.urls),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='docs'), 
     path('api/', include('rest_framework.urls')),
 ]
 
